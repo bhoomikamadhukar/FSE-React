609a63ce2c7754635846f6fdef2f3ae78bc4ca83
Bhoomika Madhukar <bhoomikamadhukar@Bhoomikas-MBP.lan>
2022-11-22 15:16:16 -0500

    Modify the tuits page

--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect }  from "react";
 import * as likeService from "../../services/likes-service";
 import * as dislikeService from "../../services/dislikes-service";
 const TuitStats = ({tuit, likeTuit = () => {},dislikeTuit=()=>{}}) => {
-    const [hasLiked, setHasLike] = useState(null);
+  const [hasLiked, setHasLike] = useState(null);
     const [hasDisliked, setHasDisliked] = useState(null);
 
     const checkLike = async() => {
@@ -26,16 +26,15 @@ const TuitStats = ({tuit, likeTuit = () => {},dislikeTuit=()=>{}}) => {
     }
 
 
-    const clickOnLike = async() => {
+    const likedByUser = async() => {
       likeTuit(tuit);
       await checkLike();
     }
 
-    const clickOnDislike = async() => {
+    const dislikedByUser = async() => {
       dislikeTuit(tuit);
       await checkDislike();
     }
-
     return (
       <div className="row mt-2">
         <div className="col">
@@ -46,38 +45,35 @@ const TuitStats = ({tuit, likeTuit = () => {},dislikeTuit=()=>{}}) => {
           <i className="far fa-retweet me-1"></i>
           {tuit.stats && tuit.stats.retuits}
         </div>
-
         <div className="col">
-          <span onClick={() => clickOnLike()}>
+          <span onClick={() => likedByUser}>
               {
-                hasLiked ? (
-                  <i className="fa-solid fa-thumbs-up"></i>
-                ) : (
-                  <i className="fa-regular fa-thumbs-up"></i>
-                )
-
+                tuit.stats && tuit.stats.likes && tuit.stats.likes > 0 &&
+                  <i className="fas fa-thumbs-up me-1" style={{color: 'red'}}></i>
               }
-
+              {
+                tuit.stats && tuit.stats.likes && tuit.stats.likes <= 0 &&
+                  <i className="far fa-thumbs-up me-1"></i>
+              }
             {tuit.stats && tuit.stats.likes}
-
           </span>
-          </div>
-            <div className="col">
-          <span onClick={() => clickOnDislike()}>
+        </div>
+        <div className="col">
+          <span onClick={() => dislikedByUser}>
               {
-                hasDisliked ? (
-                  <i className="fa-solid fa-thumbs-down"></i>
-                ) : (
-                  <i className="fa-regular fa-thumbs-down"></i>
-                )
-
+                tuit.stats && tuit.stats.dislikes && tuit.stats.dislikes > 0 &&
+                  <i className="fas fa-thumbs-down me-1" style={{color: 'black'}}></i>
               }
-
+              {
+                tuit.stats && tuit.stats.likes && tuit.stats.likes <= 0 &&
+                  <i className="far fa-thumbs-down me-1" style={{color: 'black'}}></i>
+              }
             {tuit.stats && tuit.stats.dislikes}
-
           </span>
-          </div>
-
+        </div>
+        <div className="col">
+          <i className="far fa-inbox-out"></i>
+        </div>
       </div>
     );
 }
